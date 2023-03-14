03b0c7521d7857fa86d62f539724421b9d5c454f
Brice KENGNI ZANGUIM <kenzabri2@yahoo.com>
2023-03-14 22:53:00 +0100

    Fibonacci

---
+++ .ipynb_checkpoints/Fibonacci-checkpoint.py
@@ -0,0 +1,17 @@
+#importons le module argparse qui va nous permettre d'interpréter les arguments passés sur la ligne de commande.
+
+from argparse import ArgumentParser
+
+def fibonacci(n) :
+    
+    if n <= 1 :
+        return n
+    else :
+        f_0 , f_1 = 0 , 1
+        for i in range(2, n+1) :
+            f_0 , f_1 = f_1 , f_0 + f_1
+            
+        return f_1
+    
+# de cette façon on peut utiliser la fonction fibonacci(n) en lui passant la valeur 10 en argument en utilisant le script suivant dans un terminal
+# python fibonacci.py 10
